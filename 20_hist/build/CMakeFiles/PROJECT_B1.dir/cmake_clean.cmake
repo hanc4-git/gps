312783f623e9389d513855fc156409202367ff84
Charles Han <hanc4@unlv.nevada.edu>
2022-09-12 14:52:01 -0700

    Delete 20_hist directory

--- 20_hist/build/CMakeFiles/PROJECT_B1.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PROJECT_B1"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PROJECT_B1.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
