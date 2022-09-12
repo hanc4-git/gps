19fbfb1d5144a46243cddb97b992060980bdba96
Charles Han <hanc4@unlv.nevada.edu>
2022-09-12 14:50:00 -0700

    Add files via upload

---
+++ 20_hist/build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/example.cc.o"
+  "CMakeFiles/example.dir/src/B1DetectorConstruction.cc.o"
+  "CMakeFiles/example.dir/src/B1EventAction.cc.o"
+  "CMakeFiles/example.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/example.dir/src/B1RunAction.cc.o"
+  "CMakeFiles/example.dir/src/B1SteppingAction.cc.o"
+  "example"
+  "example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
